72d7b2b74af976d732214618ee6c68a67b6c0e14
JustinAndreiGonzales <jcgonzales12@up.edu.ph>
2026-08-09 13:32:07 +0800

    select and statusbadge done

--- app/(app)/test/page.tsx
+++ app/(app)/test/page.tsx
@@ -1,31 +1,35 @@
-import Button from "@/components/Button";
-import Input from "@/components/Input";
-import TextArea from "@/components/TextArea";
+"use client";
+
+import Select from "@/components/Select";
+import { useState } from "react";
+import StatusBadge from "@/components/StatusBadge";
 
 const page = () => {
+  const VARIETALS = [
+    { value: "concord", label: "Concord" },
+    { value: "muscat", label: "Muscat" },
+    { value: "riesling", label: "Riesling" },
+    { value: "syrah", label: "Syrah" },
+    { value: "tempranillo", label: "Tempranillo" },
+  ];
+
+  const [selected, setSelected] = useState("riesling");
+
   return (
     <div className="h-4xl p-4 flex gap-md`">
-      <div>
-        <Button variant="primary" size="sm">
-          Primary
-        </Button>
-        <Button variant="primary" size="md">
-          Primary
-        </Button>
-        <Button variant="primary" size="lg">
-          Primary
-        </Button>
-      </div>
-      <Input />
-      <Input placeholder="Enter text" />
-      <Input placeholder="Enter text" label="Email" helperText="helper text" />
-      <Input
-        placeholder="Enter text"
-        label="Email"
-        state="error"
-        errorText="Wrong pw"
+      <Select
+        label="Testing"
+        options={VARIETALS}
+        value={selected}
+        onValueChange={setSelected}
+        placeholder="Select one"
       />
-      <TextArea placeholder="Enter your prompt" disabled />
+      <div>
+        <StatusBadge status="processing" />
+        <StatusBadge status="ready" />
+        <StatusBadge status="failed" />
+        <StatusBadge status="pending" />
+      </div>
     </div>
   );
 };
---
+++ components/StatusBadge.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { cn } from "./lib/utils";
+
+type Status = "pending" | "processing" | "ready" | "failed";
+
+type StatusBadgeProps = {
+  status: Status;
+};
+
+const STATUS = {
+  pending: {
+    text: "PENDING",
+    style: "bg-status-pending-bg text-status-pending-fg",
+    circle: "bg-status-pending-fg",
+  },
+  processing: {
+    text: "PROCESSING",
+    style: "bg-status-processing-bg text-status-processing-fg",
+    circle: "bg-status-processing-fg",
+  },
+  ready: {
+    text: "READY",
+    style: "bg-status-ready-bg text-status-ready-fg",
+    circle: "bg-status-ready-fg",
+  },
+  failed: {
+    text: "FAILED",
+    style: "bg-status-failed-bg text-status-failed-fg",
+    circle: "bg-status-failed-fg",
+  },
+};
+
+const StatusBadge = ({ status }: StatusBadgeProps) => {
+  return (
+    <div
+      className={cn(
+        "inline-flex items-center gap-2xs h-lg px-xs rounded-full",
+        "type-mono-label",
+        STATUS[status].style,
+      )}
+    >
+      <div
+        className={cn(
+          "h-2xs w-2xs shrink-0 rounded-full",
+          STATUS[status].circle,
+        )}
+      />
+      <p>{STATUS[status].text}</p>
+    </div>
+  );
+};
+
+export default StatusBadge;
